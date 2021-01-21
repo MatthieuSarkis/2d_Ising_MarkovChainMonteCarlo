6820466e2c3d43f9593effb2d7d927f7098b61a1
matthieu.sarkis <matthieu.sarkis@matthieusarkiss-iMac.local>
2021-01-21 15:02:18 +0900

    added files

---
+++ ising2d/ising2d/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ising2d
+//
+//  Created by matthieu.sarkis on 2021/01/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
